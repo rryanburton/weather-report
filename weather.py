173fff264701c584b901f0163fa64a8462903146
Ryan Burton <rryanburton@gmail.com>
2015-10-19 23:23:18 -0400

    more setup

---
+++ weather.py
@@ -0,0 +1,23 @@
+
+
+zipcode = input("Enter you zipcode")
+
+
+class Current:
+    pass
+
+
+class TenDay:
+    pass
+
+
+class Sunrise:
+    pass
+
+
+class Alerts:
+    pass
+
+
+class Hurricanes:
+    pass
